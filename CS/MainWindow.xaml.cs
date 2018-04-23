63950eb1d53cec7b6eba77f50e88753266616907
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Xpf.Map;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DXMap_RouteProvider {
+
+    public partial class MainWindow : Window {
+        #region #CalculateRoute
+        public MainWindow() {
+            InitializeComponent();
+            
+            // Create three waypoints and add them to a route waypoints list. 
+            List<RouteWaypoint> waypoints = new List<RouteWaypoint>();
+            waypoints.Add(new RouteWaypoint("New York", new GeoPoint(41.145556, -73.995)));
+            waypoints.Add(new RouteWaypoint("Oklahoma", new GeoPoint(36.131389, -95.937222)));
+            waypoints.Add(new RouteWaypoint("Las Vegas", new GeoPoint(36.175, -115.136389)));
+
+            routeProvider.CalculateRoute(waypoints);
+        }
+
+        private void routeProvider_LayerItemsGenerating(object sender, LayerItemsGeneratingEventArgs args) {
+            char letter = 'A';
+            
+            foreach (MapItem item in args.Items) {
+                MapPushpin pushpin = item as MapPushpin;
+                if (pushpin != null)
+                    pushpin.Text = letter++.ToString();
+            }
+        }
+
+        #endregion #CalculateRoute
+    }
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,39 @@
+﻿Imports DevExpress.Xpf.Map
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.Windows
+Imports System.Windows.Input
+
+Namespace DXMap_RouteProvider
+
+    Partial Public Class MainWindow
+        Inherits Window
+
+        #Region "#CalculateRoute"
+        Public Sub New()
+            InitializeComponent()
+
+            ' Create three waypoints and add them to a route waypoints list. 
+            Dim waypoints As New List(Of RouteWaypoint)()
+            waypoints.Add(New RouteWaypoint("New York", New GeoPoint(41.145556, -73.995)))
+            waypoints.Add(New RouteWaypoint("Oklahoma", New GeoPoint(36.131389, -95.937222)))
+            waypoints.Add(New RouteWaypoint("Las Vegas", New GeoPoint(36.175, -115.136389)))
+
+            routeProvider.CalculateRoute(waypoints)
+        End Sub
+
+        Private Sub routeProvider_LayerItemsGenerating(ByVal sender As Object, ByVal args As LayerItemsGeneratingEventArgs)
+            Dim letter As Char = "A"c
+
+            For Each item As MapItem In args.Items
+                Dim pushpin As MapPushpin = TryCast(item, MapPushpin)
+                If pushpin IsNot Nothing Then
+                    pushpin.Text = letter.ToString()
+                    letter = ChrW(AscW(letter) + 1)
+                End If
+            Next item
+        End Sub
+
+        #End Region ' #CalculateRoute
+    End Class
+End Namespace
